0a10384a07de5c6a287c402ed5bf9efc1bc3d810
Diego Cardoso <diegocar448@hotmail.com>
2025-01-15 16:46:05 -0300

    menu TopAppBar DropDownMenu bool fadeIn ou fadeOut

--- app/src/main/java/com/diego/idiomas/MainActivity.kt
+++ app/src/main/java/com/diego/idiomas/MainActivity.kt
@@ -5,15 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.diego.idiomas.ui.theme.Blue900
 import com.diego.idiomas.ui.theme.White
 
@@ -32,13 +43,40 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(){
+
+    var expandedMenu by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {},
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = Blue900
-                )
+                ),
+                actions = {
+                    Icon(
+                        imageVector = Icons.Rounded.Menu,
+                        contentDescription = "Icon Menu",
+                        tint = White,
+                        modifier = Modifier.clickable {
+                            expandedMenu = true
+                        }
+                    )
+                    //Menu do Drawable
+                    Column(
+                        modifier = Modifier
+                    ){
+                        DropdownMenu(
+                            expanded = expandedMenu,
+                            onDismissRequest = {
+                                //propriedade bool para menu aparecer ou não
+                                expandedMenu = false
+                            },
+                            modifier = Modifier.width(200.dp).background(White)
+                        )
+                        { }
+                    }
+                }
             )
         }
 
