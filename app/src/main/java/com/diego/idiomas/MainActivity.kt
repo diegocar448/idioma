51b85a2e9f2cb4d1538df3793f7dc7cbfdfc505e
Diego Cardoso <diegocar448@hotmail.com>
2025-01-16 15:52:43 -0300

    finalizado dataStore

--- app/src/main/java/com/diego/idiomas/MainActivity.kt
+++ app/src/main/java/com/diego/idiomas/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.diego.idiomas
 
+import android.app.LocaleManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -25,24 +31,30 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.os.LocaleListCompat
+import com.diego.idiomas.datasource.LanguagePreferences
 import com.diego.idiomas.ui.theme.Blue900
 import com.diego.idiomas.ui.theme.Gray900
 import com.diego.idiomas.ui.theme.White
+import kotlinx.coroutines.launch
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -58,6 +70,22 @@ class MainActivity : ComponentActivity() {
 fun Home(){
 
     var expandedMenu by remember { mutableStateOf(false) }
+    var language by remember { mutableStateOf("") }
+    var flag by remember { mutableStateOf(R.drawable.brasil) }
+    //corroutine
+    val scope = rememberCoroutineScope()
+
+    //contexto
+    val context = LocalContext.current
+
+
+    LaunchedEffect(Unit) {
+        LanguagePreferences.getLanguage(context = context).collect{
+            language = it
+            updateLocale(context, language)
+        }
+    }
+
 
     Scaffold(
         topBar = {
@@ -101,7 +129,10 @@ fun Home(){
                                     )
                                 },
                                 onClick = {
-
+                                    scope.launch {
+                                        LanguagePreferences.saveLanguage(context = context, languageCode = "pt")
+                                        expandedMenu = false
+                                    }
                                 }
                             )
                             DropdownMenuItem(
@@ -116,7 +147,10 @@ fun Home(){
                                     )
                                 },
                                 onClick = {
-
+                                    scope.launch {
+                                        LanguagePreferences.saveLanguage(context = context, languageCode = "en")
+                                        expandedMenu = false
+                                    }
                                 }
                             )
                             DropdownMenuItem(
@@ -131,7 +165,10 @@ fun Home(){
                                     )
                                 },
                                 onClick = {
-
+                                    scope.launch {
+                                        LanguagePreferences.saveLanguage(context = context, languageCode = "es")
+                                        expandedMenu = false
+                                    }
                                 }
                             )
                         }
@@ -146,8 +183,20 @@ fun Home(){
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
+            when(language){
+                "pt" -> {
+                    flag = R.drawable.bandeira1
+                }
+                "en" -> {
+                    flag = R.drawable.bandeira2
+                }
+                "es" -> {
+                    flag = R.drawable.bandeira3
+                }
+            }
+
             Image(
-                painter = painterResource(R.drawable.bandeira1),
+                painter = painterResource(flag),
                 contentDescription = null,
                 modifier = Modifier.size(200.dp)
             )
@@ -165,9 +214,17 @@ fun Home(){
                 modifier = Modifier.padding(20.dp).align(Alignment.CenterHorizontally)
                     .border(width = 2.dp, color = Blue900)
                     .padding(10.dp)
-
             )
         }
+    }
+}
+private fun updateLocale(context: Context, languageCode: String){
+    //verificação da versão do android de 13 ou superior
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+        context.getSystemService(LocaleManager::class.java)
+            .applicationLocales = LocaleList.forLanguageTags(languageCode)
+    }else{
+        AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(languageCode))
     }
 }
 
---
+++ app/src/main/java/com/diego/idiomas/datasource/LanguagePreferences.kt
@@ -0,0 +1,28 @@
+package com.diego.idiomas.datasource
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore(name = "language")
+
+object LanguagePreferences {
+
+    private val LANGUAGE_KEY = stringPreferencesKey("language_key")
+
+    suspend fun saveLanguage(context: Context, languageCode: String){
+        //edit tanto edita como atualiza também
+        context.dataStore.edit { preferences ->
+            preferences[LANGUAGE_KEY] = languageCode
+        }
+    }
+
+    fun getLanguage(context: Context): Flow<String> {
+        return context.dataStore.data.map{ preferences ->
+            preferences[LANGUAGE_KEY] ?: "pt"
+        }
+    }
+}
