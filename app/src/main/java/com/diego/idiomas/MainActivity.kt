979e9685e94b160dba14346169fdc785d59958f9
Diego Cardoso <diegocar448@hotmail.com>
2025-01-16 14:26:56 -0300

    tela inicial

--- app/src/main/java/com/diego/idiomas/MainActivity.kt
+++ app/src/main/java/com/diego/idiomas/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -26,11 +29,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.diego.idiomas.ui.theme.Blue900
+import com.diego.idiomas.ui.theme.Gray900
 import com.diego.idiomas.ui.theme.White
 
 
@@ -134,9 +142,31 @@ fun Home(){
 
     ){ paddingValues ->
         Column(
-            modifier = Modifier.fillMaxSize().background(White)
+            modifier = Modifier.fillMaxSize().background(White).padding(paddingValues),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ){
+            Image(
+                painter = painterResource(R.drawable.bandeira1),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
 
+            Text(
+                text = stringResource(R.string.idioma ),
+                fontSize = 40.sp,
+                color = Gray900,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = stringResource(R.string.descricao ),
+                fontSize = 18.sp,
+                modifier = Modifier.padding(20.dp).align(Alignment.CenterHorizontally)
+                    .border(width = 2.dp, color = Blue900)
+                    .padding(10.dp)
+
+            )
         }
     }
 }
